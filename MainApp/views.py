c78c87156c0faa7de1a08ae8c47590b5da6b7e52
Max Rudy <maxrudy@gmail.com>
2025-03-28 14:58:18 +0300

    Add comments

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,8 @@
-from django.http import Http404, HttpResponseNotFound, HttpResponse
+from django.http import Http404, HttpResponseNotFound, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, redirect, get_object_or_404
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
-from MainApp.forms import SnippetForm, UserRegistrationForm
+from MainApp.forms import SnippetForm, UserRegistrationForm, CommentForm
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
@@ -51,7 +51,8 @@ def add_snippet_page(request):
 def snippets_page(request):
     snippets = Snippet.objects.filter(public=True)
     context = {'pagename': 'Просмотр сниппетов',
-               'snippets': snippets 
+               'snippets': snippets,
+               'count': snippets.count()
                }
     return render(request, 'pages/view_snippets.html', context)
 
@@ -62,8 +63,10 @@ def snippet_detail(request, snippet_id: int):
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f"Snippet with id={snippet_id} not found")
     else:
+        comments_form = CommentForm()
         context["snippet"] = snippet
         context["type"] = "view"
+        context["comments_form"] = comments_form
         return render(request, "pages/snippet_detail.html", context)
 
 
@@ -143,6 +146,23 @@ def login(request):
 
 def logout(request):
     auth.logout(request)
-    return redirect("home")     
+    return redirect("home")   
+
+
+@login_required
+def comments_add(request):
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            snippet_id = request.POST.get("snippet_id")
+            snippet = Snippet.objects.get(id=snippet_id)
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.snippet = snippet
+            comment.save()
+            return redirect("snippet-detail", snippet_id = snippet.id)
+        return HttpResponseNotAllowed(['POST'])
+
+
 
 
